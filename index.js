067d8ee3e0d2595728d28ddc2acc53a8f151684a
timm1512 <timothy.maltby@gmail.com>
2019-06-29 08:42:57 +1000

    First Object

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ functionStore = {
 		return datesString;
 	},
 
-	convertTimestamp : function convertTimestamp(timestamp) {
+	convertTimestamp : function convertTimestamp(timestamp, format) {
 	  var d = new Date(timestamp * 1000),	// Convert the passed timestamp to milliseconds
 			yyyy = d.getFullYear(),
 			mm = ('0' + (d.getMonth() + 1)).slice(-2),	// Months are zero based. Add leading 0.
@@ -75,7 +75,11 @@ functionStore = {
 		}
 
 		// ie: 2013-02-18, 8:35 AM
-		time = dd + '-' + mm + '-' + yyyy; // + ', ' + h + ':' + min + ' ' + ampm;
+		if(format == 'day') {
+			time = dd + '-' + mm + '-' + yyyy; // + ', ' + h + ':' + min + ' ' + ampm;
+		} else {
+			time = h + ':' + min + ' ' + ampm;
+		}
 		return time;
 	}
 }
--- server.js
+++ server.js
@@ -13,34 +13,33 @@ require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
 let s = 10,
-		m = 30,
+		m = 13,
 		h = 20, //+10 to get MEL time 6AM = 20
 		dd = '*',
 		mm = '*',
-		y = '*';
+		dow = '*';
 
 let ATRange = [5, 9, 15];
 
-let clothing = {
-	"rain": [
-	{"freezing": "Jumper, breaker, thermal and gloves"},
-	{"cold": "Jumper, breaker and gloves"},
-	{"mild": "Breaker only"}
-],
-	"dry": [
-		{"freezing": "Jumper, breaker, thermal and gloves"},
-		{"cold": "Jumper, breaker and gloves"},
-		{"mild": "Jumper only"}
-	]};
+var feedbackLoop = {
+	AT: [],
+	condition: [],
+	verdict: [],
+	recordTemp: function(AT, condition) {
+		feedbackLoop.AT.push(AT);
+		feedbackLoop.condition.push(condition);
+	}
+};
 
-prepEmail;
+const GO = schedule.scheduleJob(`${s} ${m} ${h} ${dd} ${mm} ${dow}`, function() {
+	prepEmail();
+});
 
-const GO = schedule.scheduleJob(`${s} ${m} ${h} ${dd} ${mm} ${y}`, prepEmail);
-
-const prepEmail =	function() {
-	let HTMLText = '<!DOCTYPE html><html><head><style>table, th, td {border: 1px solid black;border-collapse: collapse;}th, td {padding: 5px;text-align: center;}</style></head><body>';
+function prepEmail() {
+	let HTMLText = '<!DOCTYPE html><html><head><style>table, th, td {border: 1px solid black;border-collapse: collapse;}th, td {padding: 5px;text-align: center;} .center {display:block; margin-left:auto; margin-right:auto}</style></head><body>';
 	let urlNewsBusiness = `https://newsapi.org/v2/top-headlines?country=au&category=business&apiKey=${config.apiKeyNews}`;
 	let urlWeather = `http://api.openweathermap.org/data/2.5/weather?q=Melbourne&units=metric&appid=${config.apiKeyWeather}`;
+	let urlPuppies = `https://random.dog/woof.json`;
 
 	fetch(urlWeather)
 		.then(response => response.json())
@@ -59,38 +58,60 @@ const prepEmail =	function() {
 					{"hot": "hot af"}
 				]};
 			let clothingText;
+			let condition;
 			let T = body.main.temp;
 			let v = body.wind.speed;
 			let R = body.main.humidity;
+			//SRText = functionStore.convertTimestamp(body.sys.sunrise, 'hour');
+			firstLightText = functionStore.convertTimestamp(body.sys.sunrise-20*60, 'hour');
+			//SSText = functionStore.convertTimestamp(body.sys.sunset, 'hour');
 			e = R/100*6.105*Math.exp((17.27*T/(237.7+T)));
 			let AT = T+0.33*e-0.7*v/3.6-4;
 
 			if(body.weather[0].description.includes('rain')) {
 				if(AT < ATRange[0]) {
 					clothingText = clothing.rain[0].freezing;
+					condition = 'freezing';
 				}
 			 else if(AT < ATRange[1] && AT > ATRange[0]) {
 				clothingText = clothing.rain[1].cold;
+				condition = 'cold';
 			} else if(AT < ATRange[2] && AT > ATRange[1]) {
 				clothingText = clothing.rain[2].mild;
+				condition = 'mild';
 			} else {
 				clothingText = clothing.rain[3].hot;
+				condition = 'hot';
 			}} else {
 				if(AT < ATRange[0]) {
 					clothingText = clothing.dry[0].freezing;
+					condition = 'freezing';
 				}
 			 else if(AT < ATRange[1] && AT > ATRange[0]) {
 				clothingText = clothing.dry[1].cold;
+				condition = 'cold';
 			} else if(AT < ATRange[2] && AT > ATRange[1]) {
 				clothingText = clothing.dry[2].mild;
+				condition = 'mild';
 			} else {
 				clothingText = clothing.dry[3].hot;
+				condition = 'hot';
 			}}
+
+			feedbackLoop.recordTemp(AT, condition);
+			console.log('method check AT', feedbackLoop.AT);
+			console.log('method check condition', feedbackLoop.condition);
 
 			AT = AT.toFixed(1);
 			let weatherText = `<table style="width:100%"><tr><th>Conditions</th><th>Temperature</th> <th>Humidity</th><th>Wind speed</th><th>Apparent temperature</th></tr><tr><td><a href="http://www.bom.gov.au/products/IDR024.loop.shtml">${body.weather[0].description}</a></td><td>${T} degrees</td> <td>${R} %</td><td>${v} km/hr</td><td>${AT} degrees</td></tr></table>`;
-			HTMLText = HTMLText + '<h2> Cycling weather - ' + clothingText + ' </h2> <br />';
+			HTMLText = HTMLText + '<h2> First Light at ' + firstLightText + ' - ' + clothingText + ' </h2>';
 			HTMLText = HTMLText + '<br />' + weatherText + '<br />';
+		}))
+
+	fetch(urlPuppies)
+		.then(response => response.json())
+		.then(body => new Promise(function(resolve, reject) {
+			HTMLText = HTMLText + `<img src="${body.url}" class="center" width="50%">  </img> <br />`;
 		}))
 
 	fetch(urlNewsBusiness)
@@ -108,10 +129,11 @@ const prepEmail =	function() {
 	      resolve(HTMLText);}))
 	  .then(HTMLText => new Promise(function(resolve, reject) {
 				const currentDate = new Date();
-				dateText = functionStore.convertTimestamp(currentDate/1000);
+				dateText = functionStore.convertTimestamp(currentDate/1000, 'day');
 	      let mailOptions = {
 	      from: 'timjstesting@gmail.com',
 	      to: 'timothy.maltby@gmail.com',
+				//cc: 'kashad49@gmail.com',
 	      subject: `Update for ${dateText}`,
 	      html: HTMLText +'</body></html>'
 	      };
@@ -131,4 +153,4 @@ const prepEmail =	function() {
 	      })
 	    }));
 		};
-//};
+//prepEmail();
